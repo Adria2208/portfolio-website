770f50afcfe34deaf6e0aa084f1a2cf3ebd4a448
Adria Sanchez <adrialol68@gmail.com>
2021-05-13 12:21:54 +0200

    Bootstrap, Sass and stuff like that

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css';
+import './assets/css/bootstrap.css';
+import './assets/js/bootstrap.bundle.js';
 
 export default function App() {
   return (
     <div>
-      <h1>Test</h1>
+      <h1 className='c-test'>Test</h1>
     </div>
   );
 }
