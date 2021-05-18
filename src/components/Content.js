b61b3558acf6e08353cdfc34270200697077be3c
Adria Sanchez <adrialol68@gmail.com>
2021-05-18 13:32:11 +0200

    forgot add ffs

---
+++ src/components/Content.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function Content() {
+    return (
+        <div>
+            <div className='l-content'>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque aliquam justo eu luctus varius. Aenean ornare, libero lacinia pretium tincidunt, massa nisl egestas elit, et euismod purus tortor nec augue. Sed lacinia arcu magna, eget pulvinar enim consequat in. Nam at finibus felis. Suspendisse egestas, erat ut ultrices vestibulum, velit magna suscipit lacus, id posuere ex enim vitae orci. Nam ac venenatis nisl. Donec eleifend risus in faucibus maximus. Etiam sapien libero, imperdiet ut orci varius, pretium lobortis ipsum. Aliquam erat volutpat. Donec ac massa fermentum, elementum tellus vitae, feugiat nunc. Donec vestibulum semper sollicitudin. Cras vitae fermentum erat.
+            </p>
+                <p>
+                    Phasellus sagittis viverra eros, sed lacinia lectus ultricies eu. Vivamus cursus dignissim ultrices. Cras diam lacus, pharetra in vestibulum id, tristique quis libero. Mauris convallis turpis sed urna pretium hendrerit. In tempor tellus sit amet tortor vestibulum, eu laoreet ex maximus. Quisque tincidunt cursus pellentesque. Maecenas egestas risus dolor, sit amet ultrices lorem cursus at. Vestibulum ultrices neque eu est vulputate bibendum. Nullam congue tempus nisi, vitae finibus eros sodales sed. In sed felis non libero consequat sagittis.
+            </p>
+            </div>
+        </div>
+    )
+}
---
+++ src/components/Title.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import Placeholder from '../assets/imgs/placeholder.jpg'
+
+
+export default function Title() {
+    return (
+        <div className='l-title'>
+            <div className='c-tilt'>
+                <div className='l-hoverpoints'>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-hoverpoint"></div>
+                    <div class="c-tilt__bg"></div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
